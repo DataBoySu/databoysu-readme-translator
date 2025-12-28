5c34e6b21c3d7943a1b832838a90e2c074e781b1
Anshuman Singh <148977651+DataBoySu@users.noreply.github.com>
2025-12-28 20:54:06 +0530

    Remove tests/ from repo and add to .gitignore

--- tests/test_nav.py
+++
@@ -1,8 +0,0 @@
-from readme_translator_action.translator.translate import inject_navbar
-
-
-def test_inject_navbar_creates_section():
-	readme = "# Project\n\nSome intro.\n"
-	updated = inject_navbar(readme, ['de', 'fr'])
-	assert '<!--START_SECTION:navbar-->' in updated
-	assert '[de](locales/README.de.md)' in updated
